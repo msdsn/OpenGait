5cbbf29806b3b3bf597f90635305817484375c57
msdsn <devsanms@outlook.com>
2024-11-21 03:50:55 +0300

    Fix rearrangement in BaseAttention model and update cutting parameter in configuration

--- opengait/modeling/models/baseattention.py
+++ opengait/modeling/models/baseattention.py
@@ -44,7 +44,7 @@ class BaseAttention(BaseModel):
         n, c, s, h, w = outs.size()
         x1 = rearrange(outs, 'n c s h w -> (n s) c h w')
         x1 = self.conv_reduce(x1)
-        x1 = rearrange(outs, '(n s) c h w -> (n h w) s c', n=n, s=s)
+        x1 = rearrange(x1, '(n s) c h w -> (n h w) s c', n=n, s=s)
         batch_class_token = self.class_token.expand(n*h*w, -1, -1)
         x1 = torch.cat((batch_class_token, x1), dim=1)
         x1 = self.encoder(x1)
